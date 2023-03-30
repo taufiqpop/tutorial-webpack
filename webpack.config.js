dc38cb2fc2744a6a79baab3942ff034f1dde4ad9
taufiqpop99 <taufiqpop52@gmail.com>
2023-03-30 14:41:36 +0700

    loaders

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,34 @@ module.exports = {
     },
     watch: true,
     devtool: false,
+    module: {
+        rules: [
+            // udah gak dipake
+            // {
+            //     test: /\.css$/i,
+            //     use: ['style-loader', 'css-loader'],
+            // },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS Strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles SASS to CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
 };
