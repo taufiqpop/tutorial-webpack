972e136e96d7a3fe6e3558dc1f9e96cb3bc74c1b
taufiqpop99 <taufiqpop52@gmail.com>
2023-03-31 13:56:15 +0700

    minimizer

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@ const path = require('path');
 const config = require('./webpack.config');
 const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = merge(config, {
     mode: 'production',
@@ -10,6 +12,17 @@ module.exports = merge(config, {
         filename: '[name].[contenthash].js',
         assetModuleFilename: 'img/[hash][ext]',
         clean: true,
+    },
+    optimization: {
+        minimizer: [
+            `...`,
+            new CssMinimizerPlugin(),
+            new ImageMinimizerPlugin({
+                minimizer: {
+                implementation: ImageMinimizerPlugin.squooshMinify,
+            },
+            }),
+        ],
     },
     plugins: [
         new MiniCssExtractPlugin({
